16f74d3d67252c86df7a8f4f3417254fbbf8009e
AnilEnsari <anilensari@gmail.com>
2023-08-22 21:37:32 +0300

    beforeafter added

--- src/Task.js
+++ src/Task.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { formatDistanceToNow } from "date-fns";
+import trLocale from "date-fns/locale/tr";
 import { differenceInDays } from "date-fns";
 
 const Task = ({ taskObj, onComplete }) => {
+  const beforeAfter = differenceInDays(new Date(), new Date(taskObj.date));
+  const result = formatDistanceToNow(new Date(taskObj.date), {
+    locale: trLocale,
+  });
+  console.log(result);
   const notify = () => toast("Tebrikler bir görevi daha başarıyla tamamladın!");
-  // const resultDate = () =>
-
-  const result = () => {
-    const day = differenceInDays(
-      new Date(taskObj.date.split("-").toString(",")),
-      new Date()
-    );
-    return day;
-  };
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div>son teslim:{result()}</div>
+      <div>
+        son teslim: {result}
+        {beforeAfter > 1 ? " önce" : " sonra"}
+      </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
